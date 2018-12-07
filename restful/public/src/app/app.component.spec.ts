c86d5692bdbe1dbdd30718c40748c40f32d68111
Masa Hanatate <mhanatate@gmail.com>
2018-12-07 04:19:57 -0800

    restful task update

--- restful/public/src/app/app.component.spec.ts
+++ restful/public/src/app/app.component.spec.ts
@@ -1,35 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'public'`, () => {
+  it(`should have as title 'Restful Tasks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('public');
+    expect(app.title).toEqual("Restful Tasks");
   });
 
-  it('should render title in a h1 tag', () => {
+  it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to public!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to Restful Tasks!"
+    );
   });
 });
